d2c2663560c6c1bb2bd300546397874d35aa15fb
Diego Camacho <hackvan@gmail.com>
2018-07-06 21:34:28 -0500

    Make Doctor, Patient and Appointment tests works

---
+++ lib/doctor.rb
@@ -0,0 +1,25 @@
+class Doctor
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def new_appointment(patient, date)
+    return Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    return Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def patients
+    return appointments.map { |appointment| appointment.patient }
+  end
+end
